411d7669a69422ba8fed58942eca37ccbc2f1a70
Igor Line <igor@line.ee>
2023-11-17 10:54:32 +0100

    feat: add copilot to user plugins

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,4 +1,6 @@
-return {
+local variable = os.getenv("VARIABLE_NAME")
+
+local plugins = {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
@@ -15,3 +17,43 @@ return {
   { "Shatur/neovim-ayu" },
   { "simrat39/symbols-outline.nvim", lazy = false },
 }
+
+-- Conditionally add vim-huff plugin
+if variable then
+  table.insert(plugins, 1, {
+  "zbirenbaum/copilot.lua",
+  lazy = false,
+  event = "VimEnter",
+  config = function()
+  vim.defer_fn(function()
+      require("copilot").setup {
+      panel = {
+          enabled = true,
+          auto_refresh = true,
+          ---@type table<'accept'|'next'|'prev'|'dismiss', false|string>
+          keymap = {
+          jump_prev = "[[",
+          jump_next = "]]",
+          accept = "<CR>",
+          refresh = "gr",
+          open = "<M-CR>",
+          },
+      },
+      suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          debounce = 50,
+          keymap = {
+          accept = "<M-l>",
+          next = "<M-]>",
+          prev = "<M-[>",
+          dismiss = "<C-]>",
+          },
+      },
+      }
+  end, 100)
+  end,
+})
+end
+
+return plugins
