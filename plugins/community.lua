43d8383ddc7286b566284d4a2d3fc4725cbb9884
Igor Line <igor@line.ee>
2023-11-17 10:51:21 +0100

    feat: add and use catpuccin theme

--- plugins/community.lua
+++ plugins/community.lua
@@ -8,4 +8,5 @@ return {
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.color.transparent-nvim" },
+  { import = "astrocommunity.colorscheme.catppuccin" }
 }
